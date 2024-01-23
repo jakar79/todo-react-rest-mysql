f01c29f0d84df1ea67769bf01d72c084dcbe8dfa
jakar79 <karim.jabri@gmail.com>
2024-01-23 18:20:47 +0100

    committed

--- frontend/src/component/Header.js
+++ frontend/src/component/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <>
-        <div class="header" >
+        <div className="header" >
             <div>
                 <h5> TaskList App (React NodeJs MySQL)</h5>
             </div>
